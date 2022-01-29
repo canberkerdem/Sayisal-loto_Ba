caa7922e48d45e71e19ca3a45b48b27a023b552f
canberkerdem <canberkerdem@outlook.com>
2022-01-29 18:34:47 +0300

    Arraysiz sayısalloto yapıldı

---
+++ src/com/bilgeadam/sayisalloto/CokluSartOrnek.java
@@ -0,0 +1,25 @@
+package com.bilgeadam.sayisalloto;
+
+public class CokluSartOrnek {
+
+    public static void main(String[] args) {
+
+        int sayi1 = 23;
+        int sayi2 = 35;
+        int sayi3 = 41;
+
+        //veya (or) operatörü ||
+        //ve (and) operatörü  &&
+        if (sayi3==sayi1  ||  sayi3==sayi2)
+        {
+            System.out.println("Tekrarlanan sayı var!");
+        }
+
+        while (sayi3 != sayi1 && sayi3 != sayi2)
+        {
+            System.out.println("Sayi3 diğerlerine eşit değil");
+
+            sayi3=sayi1;
+        }
+    }
+}
